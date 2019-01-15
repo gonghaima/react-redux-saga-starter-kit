6fa976f51b5222b448f110cf1d389d24f65835a8
Steven Gong <steven.gong@iag.com.au>
2019-01-16 08:11:21 +1100

    add styled-components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import styled, { ThemeProvider } from 'styled-components';
+import theme, { appColor, titleColor } from './modules/theme';
+
+const AppWrapper = styled.div`
+  display: flex;
+  background-color: ${appColor};
+  flex-direction: column;
+  min-height: 100vh;
+  opacity: 1 !important;
+  position: relative;
+  transition: opacity 0.5s;
+  h3,
+  h5 {
+    color: ${titleColor};
+  }
+`;
+
+const Main = styled.main`
+  min-height: 100vh;
+`;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <ThemeProvider theme={theme}>
+        <AppWrapper>
+          <Main>
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <p>
+                Edit <code>src/App.js</code> and save to reload.
+              </p>
+              <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Learn React Now
+              </a>
+            </header>
+          </Main>
+        </AppWrapper>
+      </ThemeProvider>
     );
   }
 }
---
+++ src/modules/theme.js
@@ -0,0 +1,33 @@
+export const headerHeight = 70;
+
+export const appColor = '#f6f6f6';
+export const mainBgColor = '#f6f8fa';
+export const borderColor = '#E5E5E5';
+export const titleColor = '#6A6A6A';
+export const subTitleColor = '#BFBFBF';
+export const focusedColor = '#272727';
+export const anchorHoverColor = '#337ab7';
+export const itemBgColor = '#ffffff';
+
+export const easing = 'cubic-bezier(0.35, 0.01, 0.77, 0.34);';
+
+export default {
+  breakpoints: [400, 768, 1024, 1280, 1920],
+  palette: {
+    primary: appColor,
+    mainBgColor,
+    borderColor,
+    anchorHoverColor
+  },
+  button: {
+    borderRadius: {
+      xs: 4,
+      lg: 28,
+      xl: 32
+    },
+    padding: {
+      lg: [12, 28],
+      xl: [14, 32]
+    }
+  }
+};
